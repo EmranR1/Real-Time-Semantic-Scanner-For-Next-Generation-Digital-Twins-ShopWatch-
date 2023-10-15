c9fa89836bcbdd5928c2497d00551c2a8d74b4da
thomaspham02 <68837202+thomaspham02@users.noreply.github.com>
2023-10-15 13:40:59 +1100

    Machine learning testing and config files

---
+++ YOLOv8 Machine Learning/main.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# https://github.com/ultralytics/ultralytics/blob/main/README.md
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="config.yaml", epochs=1)  # train the model
+
+# metrics = model.val()  # evaluate model performance on the validation set
+# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+# path = model.export(format="onnx")  # export the model to ONNX format
